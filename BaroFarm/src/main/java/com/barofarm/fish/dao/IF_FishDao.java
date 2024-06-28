8de0f183bf07d344809c57df616d49cff40bc20e
son <vicky9802@naver.com>
2024-06-28 09:04:47 +0900

    충돌해결

--- BaroFarm/src/main/java/com/barofarm/fish/dao/IF_FishDao.java
+++ BaroFarm/src/main/java/com/barofarm/fish/dao/IF_FishDao.java
@@ -15,18 +15,10 @@ public interface IF_FishDao {
 	public void delete(int no) throws Exception;
 	public FishVo modify(int no) throws Exception;
 	public void update(FishVo fVo) throws Exception;
-<<<<<<< HEAD
+	
 	public FishVo detail(String name);
 	public List<FishVo> view(String category);
 	public List<FishVo> middleview(String middlecategory);
-=======
+
 	public int getTotalCount() throws Exception;
-	public List<FishVo> fruitView();
-	public List<FishVo> fruitFruit();
-	public List<FishVo> meatView();
-	public List<FishVo> fishView();
-	public List<FishVo> fruitVege();
-	public List<FishVo> fruitGrain();
->>>>>>> 65b3721fda1a95f8563f2061e9a39a7f92f171e3
-	
 }
